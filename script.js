c1bb345510ec8ad2730fd0af99e61b87ca38e667
emanuel <emanuel.sanmelo@gmail.com>
2023-08-05 13:18:42 -0300

    atualização

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-
 const nome = document.querySelector("#nome");
 const formularioNome = document.querySelector(".formulario-nome");
 const email = document.querySelector("#email");
 const formularioemail = document.querySelector(".formulario-email");
 
-
 nome.addEventListener("focus", () => {
   formularioNome.classList.add("formulario-nome-active");
-})
+});
 nome.addEventListener("blur", () => {
   if (nome.value == "") {
     formularioNome.classList.remove("formulario-nome-active");
   }
 });
-email.addEventListener('focus',()=>{
-    formularioemail.classList.add('formulario-email-active')
-    } )
-    email.addEventListener('blur',()=>{
-        if(email.value=='')  {
-        formularioemail.classList.remove('formulario-email-active')
-    }});
+email.addEventListener("focus", () => {
+  formularioemail.classList.add("formulario-email-active");
+});
+email.addEventListener("blur", () => {
+  if (email.value == "") {
+    formularioemail.classList.remove("formulario-email-active");
+  }
+});
 
-    
+function cheacagem() {
+  console.log(nome.value, "", email.value);
+  if (email.value == "" || nome.value == "") {
+    alert ("preencha os campos");
+  } else {
+    alert("cadastrado");
+      
+  
+}}
+
+function lettersOnly(evt) {
+  if (!/[" "a-záéíóúàèìòùãõâêîôûäëïöüç]/i.test(evt.key)) return false;
+}
 
 
